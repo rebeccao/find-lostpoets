8a539ddff9d4878985e7bca351ca473f06c03fa5
rebeccao <rebecca@stylsavvy.com>
2023-07-03 16:52:27 -0600

    Added TailwindCSS. This breaks event handlers.

--- app/root.tsx
+++ app/root.tsx
@@ -1,31 +1,27 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+// ./app/root.tsx
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { LiveReload, Outlet, Links } from "@remix-run/react";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import styles from './styles/app.css';
 
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 export default function App() {
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+        />
+        <title>WTF</title>
+        <Links/>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        <Outlet/>
         <LiveReload />
       </body>
     </html>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,36 +11,11 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   const [count, setCount] = useState(0);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h2 className="text-blue-600 font-extrabold text-5xl">TailwindCSS Is Working!</h2>
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
     </div>
   );
 }
