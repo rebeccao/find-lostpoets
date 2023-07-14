5b561d5d0992402a0302b1043ba219c786598f0c
rebeccao <rebecca@stylsavvy.com>
2023-07-14 12:24:55 -0600

    Added Tailwindcss back in and fixed the issue by redoing root.tsx. Back to where I left off.

--- app/root.tsx
+++ app/root.tsx
@@ -1,27 +1,53 @@
 // ./app/root.tsx
 
-import { LiveReload, Outlet, Links } from "@remix-run/react";
-import type { MetaFunction, LinksFunction } from "@remix-run/node";
-import styles from './styles/app.css';
-
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+//import styles from './styles/app.css';
+import stylesheet from "~/tailwind.css";
+/*
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }]
-}
-export default function App() {
+}*/
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
+export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1"
-        />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>WTF</title>
+
+        {/* All meta exports on all routes will go here */}
+        <Meta />
+        
+        {/* All link exports on all routes will go here */}
         <Links/>
       </head>
       <body>
-        <Outlet/>
+        {/* Child routes go here */}
+        <Outlet />
+
+        {/* Manages scroll position for client-side transitions */}
+        {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
+        <ScrollRestoration />
+
+        {/* Script tags go here */}
+        {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
+        <Scripts />
+
+        {/* Sets up automatic reload when you change code */}
+        {/* and only does anything during development */}
+        {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
         <LiveReload />
       </body>
     </html>
